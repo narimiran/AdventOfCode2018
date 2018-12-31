6fd67d1cc0bb56574ee91ac38092e05ad9d08a68
narimiran <narimiran@disroot.org>
2018-12-31 09:51:50 +0100

    faster day 06
    * calculate area size of each point while iterating over grid
    (not afterwards in `territorySize` function)
    * put main code in a function for an additional speedup

--- nim/day06.nim
+++ nim/day06.nim
@@ -1,6 +1,6 @@
 import strutils, strscans, sequtils
 
-let input = readFile("./inputs/06.txt").splitLines
+let input = readFile("./inputs/06.txt")
 
 type
   Limits = 50..360 # manually tweaked, based on the input coordinates
@@ -11,61 +11,59 @@ iterator items(x: typedesc[Limits]): int =
   for i in x.low .. x.high:
     yield i
 
+proc read(input: string): seq[Point] =
+  var x, y: int
+  for line in input.splitLines:
+    if line.scanf("$i, $i", x, y):
+      result.add (x, y)
+
 func manhattan(p1, p2: Point): int =
   abs(p2.x - p1.x) + abs(p2.y - p1.y)
 
-let points = block:
-  var res = newSeq[Point](input.len)
-  for i, line in input:
-    var x, y: int
-    if line.scanf("$i, $i", x, y):
-      res[i] = (x, y)
-  res
-
-
-var
-  grid: Grid
-  regionSize: int
-
-for x in Limits:
-  for y in Limits:
-    var
-      minimalDistance = int.high
-      currentOwner: int
-      totalDistances: int
-    for i, point in points:
-      let distance = (x, y).manhattan(point)
-      totalDistances += distance
-      if distance < minimalDistance:
-        minimalDistance = distance
-        currentOwner = i
-      elif distance == minimalDistance:
-        currentOwner = -1
-    if currentOwner >= 0:
-      grid[y][x] = currentOwner
-    if totalDistances < 10_000:
-      inc regionSize
-
-
 proc removeInfiniteAreas(grid: Grid): auto =
-  result = {1 .. input.len}
+  result = {0 .. input.len-1}
   for n in grid[grid.low]: result.excl n
   for n in grid[grid.high]: result.excl n
   for row in grid:
     result.excl row[row.low]
     result.excl row[row.high]
 
-let finites = removeInfiniteAreas grid
 
-func territorySize(g: Grid, n: int): int =
-  for line in g:
-    result += line.count n
+proc solve =
+  let points = read input
+  var
+    grid: Grid
+    regionSize: int
+    areas = newSeq[int](points.len)
 
-var maxFiniteArea: int
-for n in finites:
-  let area = grid.territorySize(n)
-  if area > maxFiniteArea:
-    maxFiniteArea = area
+  for x in Limits:
+    for y in Limits:
+      var
+        minimalDistance = int.high
+        currentOwner: int
+        totalDistances: int
+      for i, point in points:
+        let distance = (x, y).manhattan(point)
+        totalDistances += distance
+        if distance < minimalDistance:
+          minimalDistance = distance
+          currentOwner = i
+        elif distance == minimalDistance:
+          currentOwner = -1
+      if currentOwner >= 0:
+        grid[y][x] = currentOwner
+        inc areas[currentOwner]
+      if totalDistances < 10_000:
+        inc regionSize
 
-echo maxFiniteArea
-echo regionSize
+  let finites = removeInfiniteAreas grid
+  var maxFiniteArea: int
+  for i, area in areas:
+    if i in finites and area > maxFiniteArea:
+      maxFiniteArea = area
+
+  echo maxFiniteArea
+  echo regionSize
+
+
+solve()
