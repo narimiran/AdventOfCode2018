ddae01cb4abfd2ea3b4ecaf8dcc44b61f439f7dc
narimiran <narimiran@disroot.org>
2019-06-22 14:14:02 +0200

    day03: use templates

--- nim/day03.nim
+++ nim/day03.nim
@@ -8,6 +8,17 @@ type
     id, x, y, w, h: int16
   Fabric = array[size, array[size, int16]]
 
+template forGrid(start1, size1, start2, size2: int, body: untyped) =
+  for i {.inject.} in start1 ..< start1 + size1:
+    for j {.inject.} in start2 ..< start2 + size2:
+      body
+
+template ifOverlappingFabric(start1, size1, start2, size2: int, body: untyped) =
+  forGrid(start1, size1, start2, size2):
+    if fabric[i][j] > 1:
+      body
+
+
 var
   fabric: Fabric
   claims: seq[Claim]
@@ -15,24 +26,19 @@ var
 for line in input.lines:
   var id, x, y, w, h: int
   if line.scanf("#$i @ $i,$i: $ix$i", id, x, y, w, h):
-    for i in y ..< y+h:
-      for j in x ..< x+w:
-        inc fabric[i][j]
+    forGrid(y, h, x, w):
+      inc fabric[i][j]
     claims.add (id.int16, x.int16, y.int16, w.int16, h.int16)
 
 proc first(): int =
-  for i in 0 ..< size:
-    for j in 0 ..< size:
-      if fabric[i][j] > 1:
-        inc result
+  ifOverlappingFabric(0, size, 0, size):
+    inc result
 
 proc second(): int =
   for c in claims:
     block outer:
-      for i in c.y ..< (c.y + c.h):
-        for j in c.x ..< (c.x + c.w):
-          if fabric[i][j] > 1:
-            break outer
+      ifOverlappingFabric(c.y, c.h, c.x, c.w):
+        break outer
       return c.id
 
 
