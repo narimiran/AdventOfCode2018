854f8edab331fc711aaadb65e08cff249208d847
narimiran <narimiran@disroot.org>
2020-11-29 19:46:56 +0100

    day05: use string directly

--- nim/day05.nim
+++ nim/day05.nim
@@ -3,21 +3,18 @@ import strutils
 let input = readFile("./inputs/05.txt")
 
 
-func reactions(polymer: string): seq[char] =
-  # A version with just `add` and `pop` to the `seq[char]` is more elegant,
-  # but it is 30% slower than this.
+func reactions(polymer: string): string =
   # Since lower and upper letters are 32 (2^5) characters apart, we can use
   # `a xor b`, which is faster than `abs(a - b)`.
-  var
-    stack: array[11000, char] # manually tweaked
-    pos: int
+  result = newString(12_000) # manually tweaked
+  var pos: int
   for unit in polymer:
-    if pos > 0 and (ord(unit) xor ord(stack[pos-1])) == 32:
+    if pos > 0 and (ord(unit) xor ord(result[pos-1])) == 32:
       dec pos
     else:
-      stack[pos] = unit
+      result[pos] = unit
       inc pos
-  result = stack[0 ..< pos]
+  result.setLen(pos)
 
 
 func shortest(polymer: string): int =
@@ -25,14 +22,14 @@ func shortest(polymer: string): int =
   var
     improvedPolymer: string
     polymerSize: int
-  for unit in {'a' .. 'z'}:
-    improvedPolymer = polymer.replace($unit, "").replace($unit.toUpperAscii, "")
+  for unit in 'a' .. 'z':
+    improvedPolymer = polymer.replace($unit).replace($unit.toUpperAscii)
     polymerSize = improvedPolymer.reactions.len
     if polymerSize < result:
       result = polymerSize
 
 
-let resultingPolymer = input.reactions.join
+let resultingPolymer = input.reactions
 
 echo resultingPolymer.len
 echo resultingPolymer.shortest
