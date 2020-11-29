8c64c75091587341864ef8727fc474fbcd33c36e
narimiran <narimiran@disroot.org>
2020-11-29 20:07:02 +0100

    day14: finding modulo "by hand" is faster

--- nim/day14.nim
+++ nim/day14.nim
@@ -1,18 +1,11 @@
 import strutils, sequtils
 
 const input = 765071
-let digits = toSeq($input).mapIt(it.int8 - '0'.int8)
-var
-  recipeList = newSeqUninitialized[int8](22_000_000) # manually tweaked
-  firstElf = 0
-  secondElf = 1
-  nrOfRecipes = 2
 
-recipeList[firstElf] = 3
-recipeList[secondElf] = 7
+type
+  Solution = tuple[first: string, second: int]
 
-
-proc matches(rl, digits: seq[int8]): bool =
+proc matches(rl, digits: seq[int8], nrOfRecipes: int): bool =
   for i in 1 .. 6:
     if rl[nrOfRecipes-i] != digits[6-i]:
       return false
@@ -21,25 +14,42 @@ proc matches(rl, digits: seq[int8]): bool =
 template put(rl: seq[int8], value: int8) =
   rl[nrOfRecipes] = value
   inc nrOfRecipes
-  if rl.matches digits:
-    break
+
+template move(elf: var int, digit: int8) =
+  inc elf
+  elf += digit
+  while elf >= nrOfRecipes:
+    elf -= nrOfRecipes
 
 
-while true:
-  let
+proc solve(digits: seq[int8]): Solution =
+  var
+    firstDigit, secondDigit, sum: int8
+    firstElf = 0
+    secondElf = 1
+    nrOfRecipes = 2
+    recipeList = newSeq[int8](22_000_000) # manually tweaked
+  recipeList[firstElf] = 3
+  recipeList[secondElf] = 7
+
+  while true:
     firstDigit = recipeList[firstElf]
     secondDigit = recipeList[secondElf]
-  var sum = firstDigit + secondDigit
-  if sum > 9:
-    recipeList.put 1
-    sum -= 10
-  recipeList.put sum
-  firstElf = (firstElf + firstDigit + 1) mod nrOfRecipes
-  secondElf = (secondElf + secondDigit + 1) mod nrOfRecipes
+    sum = firstDigit + secondDigit
+    if sum > 9:
+      recipeList.put 1
+      sum -= 10
+      if recipeList.matches(digits, nrOfRecipes):
+        break
+    recipeList.put sum
+    firstElf.move firstDigit
+    secondElf.move secondDigit
 
-let
-  first = recipeList[input ..< input+10].join
-  second = nrOfRecipes - 6
+  result.first = recipeList[input ..< input+10].join
+  result.second = nrOfRecipes - 6
 
-echo first
-echo second
+
+let digits = toSeq($input).mapIt(it.int8 - '0'.int8)
+let s = solve digits
+echo s.first
+echo s.second
